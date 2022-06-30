be2fdf91662d90ed6d1dce66e8d0dae8ec40d3ab
LebsterFace <samsalinsky@gmail.com>
2022-06-30 15:58:49 +0100

    locale

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
 const resultElement = document.getElementById("result");
 const dateFormatInput = document.getElementById("dateformat");
 
-// FIXME: timezone & quarter
 const getterFunctions = {
 	d: date => date.getDate(),
-	dd: date => date.toLocaleString("en", { day: "2-digit" }),
-	ddd: date => (n => n + ([, 'st', 'nd', 'rd'][n / 10 % 10 ^ 1 && n % 10] || 'th'))(date.getDate()),
+	dd: date => date.toLocaleString("en-US", { day: "2-digit" }),
+	ddd: date => (n => n + ([, "st", "nd", "rd"][n / 10 % 10 ^ 1 && n % 10] || "th"))(date.getDate()),
 
 	F: date => Math.ceil(date.getDate() / 7),
 
-	E: date => date.toLocaleString("en", { weekday: "short" }),
-	EEEE: date => date.toLocaleString("en", { weekday: "long" }),
-	EEEEE: date => date.toLocaleString("en", { weekday: "narrow" }),
-	EEEEEE: date => date.toLocaleString("en", { weekday: "long" }).slice(0, 2),
+	E: date => date.toLocaleString("en-US", { weekday: "short" }),
+	EEEE: date => date.toLocaleString("en-US", { weekday: "long" }),
+	EEEEE: date => date.toLocaleString("en-US", { weekday: "narrow" }),
+	EEEEEE: date => date.toLocaleString("en-US", { weekday: "long" }).slice(0, 2),
 
 	y: date => date.getFullYear(),
-	yy: date => date.toLocaleString("en", { year: "2-digit" }),
-	yyyy: date => date.toLocaleString("en", { year: "numeric" }).padStart(4, "0"),
+	yy: date => date.toLocaleString("en-US", { year: "2-digit" }),
+	yyyy: date => date.toLocaleString("en-US", { year: "numeric" }).padStart(4, "0"),
 
-	M: date => date.toLocaleString("en", { month: "numeric" }),
-	MM: date => date.toLocaleString("en", { month: "2-digit" }),
-	MMM: date => date.toLocaleString("en", { month: "short" }),
-	MMMM: date => date.toLocaleString("en", { month: "long" }),
-	MMMMM: date => date.toLocaleString("en", { month: "narrow" }),
+	M: date => date.toLocaleString("en-US", { month: "numeric" }),
+	MM: date => date.toLocaleString("en-US", { month: "2-digit" }),
+	MMM: date => date.toLocaleString("en-US", { month: "short" }),
+	MMMM: date => date.toLocaleString("en-US", { month: "long" }),
+	MMMMM: date => date.toLocaleString("en-US", { month: "narrow" }),
 
-	h: date => date.toLocaleString("en", { hour12: true, hour: "numeric" }).slice(0, -3),
-	hh: date => date.toLocaleString("en", { hour12: true, hour: "2-digit" }).slice(0, -3),
+	h: date => date.toLocaleString("en-US", { hour12: true, hour: "numeric" }).slice(0, -3),
+	hh: date => date.toLocaleString("en-US", { hour12: true, hour: "2-digit" }).slice(0, -3),
 
-	H: date => date.toLocaleString("en", { hour12: false, hour: "numeric" }),
-	HH: date => date.toLocaleString("en", { hour12: false, hour: "2-digit" }),
+	H: date => date.getHours(),
+	HH: date => date.getHours().toString().padStart(2, "0"),
 
 	a: date => date.getHours() < 12 ? "am" : "pm",
 	A: date => date.getHours() < 12 ? "AM" : "PM",
@@ -153,7 +152,7 @@ const generateCode = (format) => {
 		" */",
 		"const formatDate = date => {",
 		...functions,
-		'\treturn `' + returnValue + '`;',
+		"\treturn `" + returnValue + "`;",
 		"};"
 	].join("\n");
 };
